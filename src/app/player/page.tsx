f8d248c961b6568b3165a747c16a90d500175939
alanansari <ansarialan31@gmail.com>
2024-04-15 22:45:47 +0530

    FIX: broken create player page

--- src/app/player/page.tsx
+++ src/app/player/page.tsx
@@ -4,28 +4,6 @@ import CreatePlayerForm from "@/components/Player/Setup/CreatePlayerForm";
 
 function Player() {
 
-    const [data, setData] = useState({
-        name: "",
-        errMsg: "",
-        err: false
-    })
-
-    const rightName = /^[a-z ,.'-]{1,10}$/i;
-    useEffect(() => {
-        if (data.name && !rightName.test(data.name))
-            setData({
-                ...data,
-                errMsg: "Name must contain only alphabets with a max of 10 characters",
-                err: true
-            })
-        else
-            setData({
-                ...data,
-                errMsg: "",
-                err: false
-            })
-    }, [data.name])
-
     return <>
         <div className="flex flex-col justify-center items-center">
             <CheckLocalPlayer />
--- src/components/Player/Setup/CreatePlayerForm.tsx
+++ src/components/Player/Setup/CreatePlayerForm.tsx
@@ -6,6 +6,7 @@ import SelectProfile from "@/components/Player/selectProfile"
 import { toast } from 'react-toastify';
 import { useRouter } from 'next/navigation';
 import 'react-toastify/dist/ReactToastify.css';
+import { useState, useEffect } from "react";
 
 const CreatePlayerForm = () => {
 
@@ -21,25 +22,52 @@ const CreatePlayerForm = () => {
         }
     }
 
+    const [data, setData] = useState({
+        name: "",
+        errMsg: "",
+        err: false
+    })
+
+    useEffect(() => {
+        const rightName = /^[a-z,0-9,.'-]{1,20}$/i;
+        if (data.name && !rightName.test(data.name))
+            setData({
+                ...data,
+                errMsg: "Name must be alphanumeric with a max of 20 characters",
+                err: true
+            })
+        else
+            setData({
+                ...data,
+                errMsg: "",
+                err: false
+            })
+    }, [data.name])
+
   return (
     <form className="flex flex-col justify-center items-center w-full "
         action={clientAction}
     >
         <label className="text-lg text-center bg-transparent ">Enter Username</label>
-        <InputField
+        <input
             type="text"
             name="username"
             placeholder="Enter your username"
-            className="w-full border-2 border-slate-800 p-2 capitalize mb-6 focus:shadow-md"
+            className="w-full border-black border-2 focus:border-blue-600 rounded-lg outline-none md:w-4/5 text-slate-900 my-2 p-2"
             required
             autoComplete="off"
-            style="playerName"
+            value={data.name}
+            onChange={(e) => setData({
+                ...data,
+                name: e.target.value
+            })}
         />
+        {data.err && <span className="text-center text-red-500 font-Roboto text-xs">{data.errMsg}</span>}
 
         <label className="text-lg text-center mt-3 w-full bg-transparent">Select Profile Picture</label>
         <SelectProfile />
         <div className="w-full md:w-4/5">
-            <SubmitButton style="game" />
+            <SubmitButton style="game" error={data.err} />
         </div>
     </form>
   )
