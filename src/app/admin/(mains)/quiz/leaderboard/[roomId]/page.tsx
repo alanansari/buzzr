c6dcf033c66e054d0c3ea5006479f463eaede29f
Sanika goyal <sanika2110118@akgec.ac.in>
2024-04-17 22:40:45 +0530

    UPDATE: leaderboard styles

--- src/app/admin/(mains)/quiz/leaderboard/[roomId]/page.tsx
+++ src/app/admin/(mains)/quiz/leaderboard/[roomId]/page.tsx
@@ -39,7 +39,7 @@ export default async function QuizLeaderboard({ params }: { params: { roomId: st
 
             <div className="flex flex-col gap-4 my-6 overflow-x-visible">
                 {leaderboard?.length > 0 ? leaderboard.map((lead, index) => {
-                    return <div key={index} style={{ backgroundColor: colors[Math.floor(Math.random() * 8)] }} className="shadow-xl flex justify-between px-4 py-2 flex-row w-[60vw] items-center z-10">
+                    return <div key={index}  className="shadow-xl flex justify-between px-4 py-2 flex-row w-[60vw] items-center z-10 bg-white text-black">
                         {index == 0 ? <span className="text-3xl overflow-hidden">🥇</span> : index == 1 ? <span className="text-3xl overflow-hidden">🥈</span> : index == 2 ? <span className="text-3xl overflow-hidden">🥉</span> : `#${index + 1}`}
                         <div className="flex flex-row items-center gap-x-2 z-20">
                             <Image src={lead.Player.profilePic || "/avatar-1577909_1280.webp"} className="w-12 h-12 rounded-full" width={50} height={50} alt="profile pic" />
