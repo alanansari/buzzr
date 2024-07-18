d6777b48f7bc24a527034ff3b2712b0bf26ace82
Alan Ansari <ansarialan31@gmail.com>
2024-07-19 00:16:04 +0530

    UPDATE: nav links hidden on phone

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -42,7 +42,7 @@ const Footer = () => {
     <div className="flex p-2 px-8 justify-start z-50 items-center text-sm text-[#94959c] dark:text-off-white w-full absolute bottom-0">
       {Links.map((link, index) => (
         <Link href={link.link} key={index} target="_blank">
-          <div className="flex items-center justify-center mr-2">
+          <div className="hidden md:flex items-center justify-center mr-2">
             <Image 
               src={link.icon} 
               alt={link.name}
